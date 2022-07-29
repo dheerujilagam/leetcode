20633f1c18f70144c428f619059603681e24674f
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-07-29 11:51:34 +0530

    Time: 45 ms (79.39%), Space: 27.6 MB (12.32%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int search(vector<int>& vec, int target) {
+        vector<int>::iterator it;
+        it = find (vec.begin(), vec.end(), target);
+        if (it != vec.end()){
+        return it-vec.begin();
+    }
+    else
+        return -1;
+    }
+};

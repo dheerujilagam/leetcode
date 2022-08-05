f8ba586c9e9aa16f5894abc2540e3569da9a06dd
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-08-05 13:58:49 +0530

    Time: 58 ms (53.02%), Space: 27.6 MB (12.40%) - LeetHub

--- 704-binary-search/704-binary-search.cpp
+++ 704-binary-search/704-binary-search.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int search(vector<int>& vec, int target) {
-        vector<int>::iterator it;
-        it = find (vec.begin(), vec.end(), target);
-        if (it != vec.end()){
-            return it-vec.begin();
+        int l=0,r=vec.size()-1;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(vec[mid]==target)
+                return mid;
+            else if(vec[mid]>target)
+                r=mid-1;
+            else
+                l=mid+1;
         }
-        else
-            return -1;
+        return -1;
     }
 };

1c92c200321b36a06c6c4437d8b4c7d54297ac26
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-08-10 18:24:01 +0530

    Time: 19 ms (31.43%), Space: 6 MB (77.20%) - LeetHub

---
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -0,0 +1,79 @@
+/*
+M  1000
+CM 900
+D  500
+CD 400
+C  100
+XC 90
+L  50
+XL 40
+X  10
+IX 9
+V  5
+IV 4
+I  1
+*/
+class Solution {
+public:
+    string romans(int n,string &s){
+        while(n){
+            if(n>=1000){
+                s+="M";
+                n-=1000;
+            }
+            else if(n>=900){
+                s+="CM";
+                n-=900;
+            }
+            else if(n>=500){
+                s+="D";
+                n-=500;
+            }
+            else if(n>=400){
+                s+="CD";
+                n-=400;
+            }
+            else if(n>=100){
+                s+="C";
+                n-=100;
+            }
+            else if(n>=90){
+                s+="XC";
+                n-=90;
+            }
+            else if(n>=50){
+                s+="L";
+                n-=50;
+            }
+            else if(n>=40){
+                s+="XL";
+                n-=40;
+            }
+            else if(n>=10){
+                s+="X";
+                n-=10;
+            }
+            else if(n>=9){
+                s+="IX";
+                n-=9;
+            }
+            else if(n>=5){
+                s+="V";
+                n-=5;
+            }
+            else if(n>=4){
+                s+="IV";
+                n-=4;
+            }
+            else if(n>=1){
+                s+="I";
+                n-=1;
+            }
+        }
+        return s;
+    }
+    string intToRoman(int num) {
+        string s="";
+        return romans(num,s);
+    }
+};

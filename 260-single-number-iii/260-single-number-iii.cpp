29edb8e3c0b118dec119607b8456f1651d097b43
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-08-21 21:09:29 +0530

    Time: 5 ms (96.20%), Space: 10 MB (78.33%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.cpp
+++ 260-single-number-iii/260-single-number-iii.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    vector<int> singleNumber(vector<int>& nums) {
-        map<int,int>mp;
-        for(auto i:nums)
-            mp[i]++;
-        vector<int>ans;
-        for(auto it:mp){
-            if(it.second==1)
-                ans.push_back(it.first);
+    int pos(int val){
+        int ind=0;
+        while(val){
+            if(val&1)
+                return ind;
+            val=val>>1;
+            ind++;
         }
-        return ans;
+        return ind;
+    }
+    vector<int> singleNumber(vector<int>& nums) {
+        int val=0;
+        for(auto i:nums)
+            val=val^i;
+        int t=val;
+        int index=pos(t);
+        for(int i=0;i<nums.size();i++){
+            int temp=1<<index;
+            if(nums[i]&temp)
+                t^=nums[i];
+        }
+        return {t,t^val};
     }
 };

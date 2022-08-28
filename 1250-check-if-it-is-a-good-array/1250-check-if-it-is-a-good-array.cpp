dc0490eca736ae55d021deefe7d7907b5c90b131
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-08-28 15:13:02 +0530

    Time: 52 ms (92.11%), Space: 29.3 MB (69.84%) - LeetHub

--- 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
+++ 1250-check-if-it-is-a-good-array/1250-check-if-it-is-a-good-array.cpp
@@ -4,6 +4,8 @@ public:
         int val=0;
         for(int i=0;i<nums.size();i++){
             val=gcd(val,nums[i]);
+            if(val==1)
+                return 1;
         }
         if(val==1)
             return 1;

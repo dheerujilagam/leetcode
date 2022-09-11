4aa7e70b45c0c36f28fa15cb7d56e3f9935b6836
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-09-11 08:15:46 +0530

    Time: 228 ms (14.79%), Space: 36.9 MB (34.51%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& s, vector<int>& e, int k) {
+        long long mod=1e9+7;
+        vector<pair<int,int>>vp;
+        for(int i=0;i<n;i++)
+            vp.push_back({e[i],s[i]});
+        sort(vp.begin(),vp.end());
+        priority_queue<int,vector<int>,greater<int>>pq;
+        long long t=0,mx=0;
+        for(int i=n-1;i>=0;i--){
+            t+=vp[i].second;
+            pq.push(vp[i].second);
+            while(pq.size()>k){
+                t-=pq.top();
+                pq.pop();
+            }
+            mx=max(mx,(t*vp[i].first));
+        }
+        return mx%mod;
+    }
+};

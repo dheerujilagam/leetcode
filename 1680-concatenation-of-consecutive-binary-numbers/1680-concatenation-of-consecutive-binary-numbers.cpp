d7fb270c2d2f4f0e4c1f43ce643b88e7416c7d3a
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-09-23 10:51:11 +0530

    Time: 62 ms (71.74%), Space: 5.9 MB (82.61%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
-    string DToB(int num){
-        string str;
+    int DToB(int num){
+        int str=0;
         while(num){
-            if(num & 1)
-                str+='1';
-            else
-                str+='0';
+            str++;
             num>>=1; 
         }   
         return str;
@@ -14,8 +11,8 @@ public:
     int concatenatedBinary(int n) {
         long long ans=0,mod=1000000007;
         for(int i=1;i<=n;i++){
-            string s=DToB(i);
-            ans=(((ans<<s.size())%mod)+i)%mod;
+            int l=DToB(i);
+            ans=((ans<<l%mod)+i)%mod;
         }
         return ans;
     }

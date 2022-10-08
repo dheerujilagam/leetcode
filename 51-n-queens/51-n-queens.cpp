bcfcfd53f1ae9911c12a28b12283cc504f2454da
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-10-08 08:13:47 +0530

    Time: 189 ms (5.02%), Space: 50.5 MB (5.68%) - LeetHub

---
+++ 51-n-queens/51-n-queens.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool boundary(int i, int j, int n){
+        if(i<0 or j<0 or i>=n or j>=n)
+            return false;
+        return true;
+    }
+    bool issafe(int row, int col, vector<string>t, int n){
+        for(int i=0;i<n;i++){
+            if(t[i][col]=='Q' or t[row][i]=='Q')
+                return false;
+        }
+        for(int j=0;j<n;j++){
+            if(boundary(row-j,col-j,n) and t[row-j][col-j]=='Q')
+                return false;
+            if(boundary(row+j,col-j,n) and t[row+j][col-j]=='Q')
+                return false;
+            if(boundary(row-j,col+j,n) and t[row-j][col+j]=='Q')
+                return false;
+            if(boundary(row+j,col+j,n) and t[row+j][col+j]=='Q')
+                return false;
+        }
+        return true;
+    }
+    void rec(vector<vector<string>>&ans, vector<string>t, int n, int row){
+        if(row==n){
+            ans.push_back(t);
+            return ;
+        }
+        for(int i=0;i<n;i++){
+            if(issafe(row,i,t,n)){
+                t[row][i]='Q';
+                rec(ans,t,n,row+1);
+                t[row][i]='.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        if(n==1){
+            vector<string>v(1,"Q");
+            vector<vector<string>>a(1,v);
+            return a;
+        }
+        if(n==2 || n==3){
+            vector<vector<string>>a;
+            return a;
+        }
+        string s(n,'.');
+        vector<string>t(n,s);
+        vector<vector<string>>ans;
+        rec(ans,t,n,0);
+        return ans;
+    }
+};

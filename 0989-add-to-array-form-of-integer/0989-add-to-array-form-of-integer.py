513c442e785f2acbc42cbf834ade4114a00df3b6
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-10-14 10:00:20 +0530

    Time: 814 ms (20.61%), Space: 15.1 MB (27.16%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,8 +1,11 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        s=""
+        val=0
         for i in num:
-            s+=str(i)
-        ans=int(s)+k
-        ans=list(str(ans))
-        return ans;
+            val=(val*10)+i
+        val+=k
+        ans=[]
+        while val:
+            ans.append(val%10)
+            val=val//10
+        return ans[::-1]

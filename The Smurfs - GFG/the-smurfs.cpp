16530bb63bf8a47c7262beca2369eeddf607b2f2
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2022-10-27 20:53:38 +0530

    Added solution - LeetHub

---
+++ The Smurfs - GFG/the-smurfs.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int findMin(int n, char a[]){
+        // code here
+        int r=0,b=0,g=0;
+        for(int i=0;i<n;i++){
+            if(a[i]=='B'){
+                b++;
+            }
+            else if(a[i]=='R'){
+                r++;
+            }
+            else
+                g++;
+        }
+        if((r%2==0 and g%2==0 and b%2==0) or (r&1 and g&1 and b&1))
+            return 2;
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        char a[n];
+        for(int i = 0;i < n;i++)
+            cin>>a[i];
+        
+        Solution ob;
+        cout<<ob.findMin(n, a)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

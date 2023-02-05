8a683ab18bda9709baf7660be0cc4682ead49286
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-02-05 21:39:55 +0530

    Time: 15 ms (76.14%), Space: 8.5 MB (92.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    vector<int> findAnagrams(string txt, string pat) {
-        if(txt.size() < pat.size())return {};
-	    int N = txt.size();
-	    int k = pat.size();
-	    vector<int> res;
-	    vector<int> txtM(26,0);
-	    vector<int> patM(26,0);
-	    int j = 0;
-	    for(int i = 0; i < k; i++){
-	        txtM[txt[i]-'a']++;
-	        patM[pat[i]-'a']++;
-	    }
-	    if(txtM == patM)res.push_back(0);
-	    for(int i = k; i < N; i++){
-	        txtM[txt[j]-'a']--;
-	        txtM[txt[i]-'a']++;
+    vector<int> findAnagrams(string s, string p) {
+        int n=s.size(),m=p.size();
+        if(n<m)
+            return {};
+        vector<int>res;
+        vector<int>v1(26,0),v2(26,0);
+        for(int i=0;i<m;i++){
+            v1[s[i]-'a']++;
+            v2[p[i]-'a']++;
+        }
+        if(v1==v2)
+            res.push_back(0);
+        int j=0;
+        for(int i=m;i<n;i++){
+            v1[s[j]-'a']--;
+            v1[s[i]-'a']++;
             j++;
-	        if(txtM == patM)res.push_back(j);
-	    }
-	    return res;
-	
+            if(v1==v2)
+                res.push_back(j);
+        }
+        return res;
     }
 };

f6660e87a3a2e8f88e72e88e011b14f2a5cb3e44
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-02-12 22:18:23 +0530

    Added solution - LeetHub

---
+++ Shortest Unique prefix for every word - GFG/shortest-unique-prefix-for-every-word.cpp
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    struct TrieNode{
+    	TrieNode *child[26];
+    	int cnt;
+    	TrieNode(){
+    	    cnt=0;
+    		for(int i=0;i<26;i++)
+    			child[i]=NULL;
+    	}
+    };
+    
+    void insert(TrieNode* root, string &key){
+    	TrieNode* cur=root;
+    	for(char it:key){
+    		if(cur->child[it-'a']==NULL){
+    			cur->child[it-'a']=new TrieNode();
+    		}
+    		cur->cnt++;
+    		cur=cur->child[it-'a'];
+    	}
+    }
+    
+    string search(TrieNode* root, string &key){
+    	TrieNode* cur=root;
+    	string res="";
+    	for(char it:key){
+    		res+=it;
+    		if(cur->child[it-'a']->cnt==1)
+    		    break;
+    		cur=cur->child[it-'a'];
+    	}
+    	return res;
+    }
+
+    vector<string> findPrefixes(string arr[], int n)
+    {
+        //code here
+        TrieNode* root=new TrieNode();
+        for(int i=0;i<n;i++)
+            insert(root,arr[i]);
+        vector<string>ans;
+        for(int i=0;i<n;i++){
+            ans.push_back(search(root,arr[i]));
+        }
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+            
+        Solution ob;
+        vector<string> ans = ob.findPrefixes(arr, n);
+        
+        for (int i = 0; i < ans.size(); i++)
+            cout << ans[i] <<" ";
+
+
+        cout << "\n";
+
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends

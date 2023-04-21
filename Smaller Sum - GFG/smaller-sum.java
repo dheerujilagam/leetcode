4cf2cc29344fb6ffb53605f3f7f5af36240c7e4d
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-04-22 00:44:39 +0530

    Added solution - LeetHub

---
+++ Smaller Sum - GFG/smaller-sum.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.util.*;
+import java.io.*;
+
+public class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(in.readLine());
+            String s[]=in.readLine().trim().split(" ");
+            int a[]=new int[n];
+            for(int i=0;i<n;i++){
+                a[i]=Integer.parseInt(s[i]);
+            }
+            Solution ob=new Solution();
+            long ans[]=ob.smallerSum(n,a);
+            for(long i:ans){
+                out.print(i+" ");
+            }out.println();
+        }
+        out.close();
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public long[] smallerSum(int n,int arr[])
+    {
+        long [] dp = new long [100002];
+        for(int it : arr) dp[it + 1] += it;
+        for(int i = 2; i <= 100001; i++) dp[i] += dp[i - 1];
+        long [] ans = new long [n];
+        for(int i = 0; i < n; i++) ans[i] = dp[arr[i]];
+        return ans;
+    }
+}

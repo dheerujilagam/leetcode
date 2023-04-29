9e10a4fc4a9e1438b12063c6296498f9a95ecdc6
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-04-29 21:25:51 +0530

    Added solution - LeetHub

---
+++ Find Number - GFG/find-number.java
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int t=Integer.parseInt(in.readLine());
+        while(t-->0){
+            
+            long n=Long.parseLong(in.readLine());
+            Solution ob=new Solution();
+            out.println(ob.findNumber(n));
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+class Solution
+{
+    public int[] pos = {1, 3, 5, 7, 9};
+    public long findNumber(long n) {
+        if(n <= 5) return pos[(int)n - 1];
+        if(n % 5 == 0) return findNumber(n / 5 - 1) * 10 + 9;
+        return findNumber(n / 5) * 10 + pos[(int)(n % 5) - 1];
+    }
+}

0d00d0f13c7c9d2dfeaab09a33b4b3e2c121b70d
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-04-29 21:11:30 +0530

    Added solution - LeetHub

--- Find Number - GFG/find-number.cpp
+++ Find Number - GFG/find-number.cpp
@@ -12,11 +12,7 @@ class Solution{
 public:
     int pos[5] = {1, 3, 5, 7, 9};
     long long findNumber(long long n){
-        if(n == 1) return 1;
-        if(n == 2) return 3;
-        if(n == 3) return 5;
-        if(n == 4) return 7;
-        if(n == 5) return 9;
+        if(n <= 5) return pos[n - 1];
         if(n % 5 == 0) return findNumber(n / 5 - 1) * 10 + 9;
         return findNumber(n / 5) * 10 + pos[n % 5 - 1];
     }

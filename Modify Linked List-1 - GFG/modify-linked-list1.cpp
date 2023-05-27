a44c5b4d6ca5ca3ccb3a222712127f6d5b8134aa
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-05-27 09:41:45 +0530

    Added solution - LeetHub

---
+++ Modify Linked List-1 - GFG/modify-linked-list1.cpp
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node* modifyTheList(struct Node *head);
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *head)
+{
+    Node *temp=head;
+	while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+class Solution{
+    public:
+    Node* reverseLinkedList(Node* head) {
+        Node* prev = nullptr;
+        Node* curr = head;
+        while (curr) {
+            Node* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        return prev;
+    }
+    struct Node* modifyTheList(struct Node *head)
+    {
+        Node* slow = head, *fast = head;
+        while(fast && fast -> next) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        Node* prev = head;
+        while(prev -> next != slow) prev = prev -> next;
+        prev -> next = reverseLinkedList(prev -> next);
+        Node* cur = head, *mid = prev -> next;
+        while(cur != prev -> next && mid) {
+            int x = cur -> data;
+            cur -> data = mid -> data - cur -> data;
+            mid -> data = x;
+            cur = cur -> next;
+            mid = mid -> next;
+        }
+        prev -> next = reverseLinkedList(prev -> next);
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		struct Node *head = NULL,*temp=NULL;
+		while(n--){
+		    int a;
+            cin>>a;
+			Node *newNode = new Node(a);
+			if(head==NULL)
+                head=newNode;
+            else
+                temp->next=newNode;
+            temp=newNode;
+		}
+		Solution obj;
+		head = obj.modifyTheList(head);
+		print(head);
+		
+	}
+    return 0;
+}
+// } Driver Code Ends

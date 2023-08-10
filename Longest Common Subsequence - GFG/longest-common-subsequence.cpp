382b7e2d09c0f69ad231290748bf7b0b4d9e1404
Subhash Jilagam <81238961+dheerujilagam@users.noreply.github.com>
2023-08-10 19:50:38 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -10,22 +10,17 @@ class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int lcs(int x, int y, string s1, string s2)
+    int solve(int i, int j, int n, int m, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(i == n || j == m) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s1[i] == s2[j]) return dp[i][j] = 1 + solve(i + 1, j + 1, n, m, s1, s2, dp);
+        return dp[i][j] = max(solve(i + 1, j, n, m, s1, s2, dp), solve(i, j + 1, n, m, s1, s2, dp));
+    }
+    int lcs(int n, int m, string s1, string s2)
     {
-        int dp[x+1][y+1];
-        for(int i=0;i<=x;i++)
-        dp[i][0]=0;
-        for(int i=0;i<=y;i++)
-        dp[0][i]=0;
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=y;j++){
-                if(s1[i-1]==s2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-        return dp[x][y];
+        // your code here
+        vector<vector<int>> dp(n, vector<int> (m, -1));
+        return solve(0, 0, n, m, s1, s2, dp);
     }
 };
 
@@ -33,15 +28,15 @@ class Solution
 //{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
